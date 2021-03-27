53831e6dcc7ec3c9dffd931d2a23dc1af57157eb
devstefancho <skku7714@gmail.com>
2021-03-27 22:29:00 +0900

    chat app complete

--- script.js
+++ script.js
@@ -2,20 +2,45 @@ const socket = io('http://localhost:3030');
 
 const messageInput = document.getElementById('message-input');
 const messageContainer = document.getElementById('message-container');
+const myStyle = {
+    backgroundColor: '#212121',
+    color: '#fff'
+};
+const newUserStyle = {
+    backgroundColor: 'grey',
+    color: '#fff',
+    textAlign: 'center',
+    padding: '1rem'
+};
+
+const appendMessage = (message, name, self = false) => {
+    const newMessage = document.createElement('div');
+    newMessage.innerHTML = `${name}: ${message}`;
+    if(self) {
+        Object.assign(newMessage.style, myStyle);
+    }
+    messageContainer.appendChild(newMessage);
+}
+
+const name = prompt('Enter your name');
+socket.emit('new-user', name);
 
 document.addEventListener('submit', (e) => {
     e.preventDefault();
     const message = messageInput.value;
     socket.emit('send-chat-msg', message);
     messageInput.value = '';
+
+    appendMessage(message, name, true); // display my message
 })
 
-socket.on('chat-message', data => {
-    console.log(data)
+socket.on('response-msg', ({ message, name }) => {
+    appendMessage(message, name); // display others message
 })
 
-socket.on('response-msg', message => {
+socket.on('new-user', name => {
     const newMessage = document.createElement('div');
-    newMessage.innerHTML = `Your Message: ${message}`;
+    newMessage.innerHTML = `${name} joined`;
+    Object.assign(newMessage.style, newUserStyle);
     messageContainer.appendChild(newMessage);
 })
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const io = require('socket.io')(3030)
 
-io.on('connection', socket => {
-    console.log('new user')
-    socket.emit('chat-message', 'Hello world')
+const users = {};
 
+io.on('connection', socket => {
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('new-user', name);
+    })
     socket.on('send-chat-msg', msg => {
         console.log('message: ', msg);
-        socket.emit('response-msg', msg);
+        socket.broadcast.emit('response-msg', { message: msg, name: users[socket.id] });
     })
 })
