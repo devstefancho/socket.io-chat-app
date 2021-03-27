50d2fde30572c652c24f9cdd23d6409c9f5f5e69
devstefancho <skku7714@gmail.com>
2021-03-27 18:57:32 +0900

    display message

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const socket = io('http://localhost:3030');
 
+const messageInput = document.getElementById('message-input');
+const messageContainer = document.getElementById('message-container');
+
+document.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    socket.emit('send-chat-msg', message);
+    messageInput.value = '';
+})
 
 socket.on('chat-message', data => {
-    console.log('hohoho')
     console.log(data)
+})
+
+socket.on('response-msg', message => {
+    const newMessage = document.createElement('div');
+    newMessage.innerHTML = `Your Message: ${message}`;
+    messageContainer.appendChild(newMessage);
 })
--- server.js
+++ server.js
@@ -3,4 +3,9 @@ const io = require('socket.io')(3030)
 io.on('connection', socket => {
     console.log('new user')
     socket.emit('chat-message', 'Hello world')
+
+    socket.on('send-chat-msg', msg => {
+        console.log('message: ', msg);
+        socket.emit('response-msg', msg);
+    })
 })
